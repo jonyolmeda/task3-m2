71c5f97607ba7a1a7249795819165a1642683167
jonyolmeda <jonyolmeda@gmail.com>
2022-10-17 22:48:53 -0300

    Rama task 3 modificada e incompleta

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const tarjetas = document.getElementById("contenedor");
-const input = document.getElementById("input-texto")
-const checkbox = document.getElementById(`checkbox`)
+const input = document.getElementById("input-texto");
+const checkbox = document.getElementById(`checkbox`);
 
 //Cree una función para imprimir las tarjetas en la página Home.
-function imprimir (array,contenedor){
-  array.forEach (e => {contenedor.innerHTML += `
+function imprimir(array, contenedor) {
+  array.forEach((e) => {
+    contenedor.innerHTML += `
   <div class="card col-3 bg-black">
       <div class="card-body">
           <p class="card-text text-light">${e.name}</p>
@@ -17,61 +18,65 @@ function imprimir (array,contenedor){
       <a href="details.html" class="btn btn-danger">Details</a>
   </div>
 `;
-})
-};
-imprimir(events,tarjetas)
- //Función creada para arrojar un mensaje cuando no existe coincidencia.
+  });
+}
+imprimir(events, tarjetas);
+//Función creada para arrojar un mensaje cuando no existe coincidencia.
 
- function notCoincidence(array, contenedor){
-  if(array <= 0){
-    contenedor.innerHTML=
-    `
+function notCoincidence(array, contenedor) {
+  if (array <= 0) {
+    contenedor.innerHTML = `
     <h2>Sin coincidencias</h2>
-    `
+    `;
   }
 }
 //      INPUT
 //Por medio del .filter filtro los nombres de los eventos; le doy un espacio vacío y aplico la función para que no haya coincidencia para después imprimirlas
 
-input.addEventListener(`keyup`, e => {
-  elementosFiltrados = events.filter(nombres => nombres.name.toLowerCase().includes(e.target.value.toLowerCase()))
-  tarjetas.innerHTML = ''
-  notCoincidence(elementosFiltrados, tarjetas)
-  imprimir(elementosFiltrados,tarjetas)
-})
+input.addEventListener(`keyup`, (e) => {
+  elementosFiltrados = events.filter((nombres) =>
+    nombres.name.toLowerCase().includes(e.target.value.toLowerCase())
+  );
+  tarjetas.innerHTML = "";
+  notCoincidence(elementosFiltrados, tarjetas);
+  imprimir(elementosFiltrados, tarjetas);
+});
 
 //      CHECKBOX
 
-let categorias = Array.from(new Set(events.map(objeto => objeto.category)))
+let categorias = Array.from(new Set(events.map((objeto) => objeto.category)));
 
-categorias.forEach(e => {
-  checkbox.innerHTML +=
-  `
+categorias.forEach((e) => {
+  checkbox.innerHTML += `
     <div class="form-check form-check-inline" id="checkbox">
-      <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="${e}">
+      <input class="form-check-input" type="checkbox" id="${e}" value="${e}">
       <label class="form-check-label text-light" for="inlineCheckbox1">${e}</label>
     </div>
-  `
+  `;
 });
 
-let listaChequeada = []
+let listaChequeada = [];
 
-checkbox.addEventListener(`change`, e =>{
-    if (e.target.checked) {
-        listaChequeada = listaChequeada.concat(events.filter(evento=> evento.category.toLowerCase().includes(e.target.id.toLowerCase())))
-        console.log(listaChequeada);
-        tarjetas.innerHTML = ''
-        imprimir(listaChequeada, tarjetas)
-    }
+checkbox.addEventListener(`change`, (e) => {
+  if (e.target.checked) {
+    listaChequeada = listaChequeada.concat(
+      events.filter((evento) =>
+        evento.category.toLowerCase().includes(e.target.id.toLowerCase())
+      )
+    );
+    console.log(listaChequeada);
+    tarjetas.innerHTML = "";
+    imprimir(listaChequeada, tarjetas);
+  } else if (!e.target.checked) {
+    listaChequeada = listaChequeada.filter(
+      (evento) =>
+        !evento.category.toLowerCase().includes(e.target.id.toLowerCase())
+    );
+    tarjetas.innerHTML = "";
+    imprimir(listaChequeada, tarjetas);
+  }
 
-   else if(!e.target.checked){
-    listaChequeada = listaChequeada.filter(evento => !evento.category.toLowerCase().includes( e.target.id.toLowerCase() ) )
-    tarjetas.innerHTML = ''
-    imprimir(listaChequeada, tarjetas)
-    }
-
-    if (listaChequeada.length === 0){
-      imprimir(events,tarjetas)
-      }
-    }
-  )
+  if (listaChequeada.length === 0) {
+    imprimir(events, tarjetas);
+  }
+});
--- upcomingevents.js
+++ upcomingevents.js
@@ -1,53 +1,86 @@
-//Cree una función del array de eventos.
-function dataJs() {
-  return datos.events
-}
+const tarjetas = document.getElementById("contenedor");
+const input = document.getElementById("input-texto");
+const checkbox = document.getElementById(`checkbox`);
 
 //Cree una función para imprimir las tarjetas en la página Home.
-function impresionTarjeta(elemento, datos) {
-  let contenedor = document.getElementById(`contenedor`);
-  for (let info of datos.events) {
-    if (datos.currentDate < info.date) {
-      elemento.innerHTML += `
-    <div class="card col-3 bg-black">
-        <div class="card-body">
-            <p class="card-text text-light">${info.name}</p>
-        </div>
-        <img src="${info.image}" alt="${info.name}">
-        <div class="card-body">
-            <p class="card-text text-light">${info.description}t.</p>
-            <p class="card-text text-light">Price: $${info.price}</p>
-        </div>
-        <a href="details.html" class="btn btn-danger">Details</a>
-    </div>
+//if (currentDate > events.date) {
+function imprimir(array, contenedor) {
+  array.forEach((e) => {
+    if (currentDate < events.date) {
+      contenedor.innerHTML += `
+  <div class="card col-3 bg-black">
+      <div class="card-body">
+          <p class="card-text text-light">${e.name}</p>
+      </div>
+      <img src="${e.image}" alt="${e.name}">
+      <div class="card-body">
+          <p class="card-text text-light">${e.description}t.</p>
+          <p class="card-text text-light">Price: $${e.price}</p>
+      </div>
+      <a href="details.html" class="btn btn-danger">Details</a>
+  </div>
 `;
+      imprimir(events, tarjetas);
     }
+  });
+}
+
+//Función creada para arrojar un mensaje cuando no existe coincidencia.
+
+function notCoincidence(array, contenedor) {
+  if (array <= 0) {
+    contenedor.innerHTML = `
+    <h2>Sin coincidencias</h2>
+    `;
   }
 }
-impresionTarjeta(contenedor, datos);
+//      INPUT
+//Por medio del .filter filtro los nombres de los eventos; le doy un espacio vacío y aplico la función para que no haya coincidencia para después imprimirlas
 
-//Asigne una variable a las categorias del array.
-
-let categorias = datos.events.map (categories => categories.category)
-
-//Con la fn .Sort lo acomodé alfabeticamente y con new Set se eliminan los elementos repetidos.
-
-categorias.sort()
-
-categorias = new Set(categorias)
-
-//Recorro el array categorias con la fn .forEach e imprimo los checkbox.
-
-const checkbox = document.getElementById(`checkbox`)
-categorias.forEach(e => {
-  checkbox.innerHTML +=
-  `
-    <div class="form-check form-check-inline" id="checkbox">
-      <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="${e}">
-      <label class="form-check-label text-light" for="inlineCheckbox1">${e}</label>
-    </div>
-  `
+input.addEventListener(`keyup`, (e) => {
+  elementosFiltrados = events.filter((nombres) =>
+    nombres.name.toLowerCase().includes(e.target.value.toLowerCase())
+  );
+  tarjetas.innerHTML = "";
+  notCoincidence(elementosFiltrados, tarjetas);
+  imprimir(elementosFiltrados, tarjetas);
 });
 
+//      CHECKBOX
 
-//
+let categorias = Array.from(new Set(events.map((objeto) => objeto.category)));
+
+categorias.forEach((e) => {
+  checkbox.innerHTML += `
+    <div class="form-check form-check-inline" id="checkbox">
+      <input class="form-check-input" type="checkbox" id="${e}" value="${e}">
+      <label class="form-check-label text-light" for="inlineCheckbox1">${e}</label>
+    </div>
+  `;
+});
+
+let listaChequeada = [];
+
+checkbox.addEventListener(`change`, (e) => {
+  if (e.target.checked) {
+    listaChequeada = listaChequeada.concat(
+      events.filter((evento) =>
+        evento.category.toLowerCase().includes(e.target.id.toLowerCase())
+      )
+    );
+    console.log(listaChequeada);
+    tarjetas.innerHTML = "";
+    imprimir(listaChequeada, tarjetas);
+  } else if (!e.target.checked) {
+    listaChequeada = listaChequeada.filter(
+      (evento) =>
+        !evento.category.toLowerCase().includes(e.target.id.toLowerCase())
+    );
+    tarjetas.innerHTML = "";
+    imprimir(listaChequeada, tarjetas);
+  }
+
+  if (listaChequeada.length === 0) {
+    imprimir(events, tarjetas);
+  }
+});
